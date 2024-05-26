94af2905e7131e84a86131c871f3c99e4eba6b7b
Yoga 7i <abhi290392@gmail.com>
2024-05-26 13:25:47 +0530

    changes to move kafka and spark config to sparkcommon

---
+++ src/main/java/com/example/spark/config/SparkConfig.java
@@ -0,0 +1,39 @@
+package com.example.spark.config;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.sql.SparkSession;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class SparkConfig{
+    
+    @Bean
+    public SparkConf  sparkConf() {
+        SparkConf conf= new SparkConf().setAppName("Java Spark").setMaster("local[*]");
+        return conf;
+        
+    }
+
+    @Bean
+    public SparkSession sparkSession(){
+        SparkSession spark = SparkSession
+            .builder()
+            .config(sparkConf())
+            .getOrCreate();
+
+        return spark;
+    }
+
+
+
+    
+
+    
+    
+     
+}
